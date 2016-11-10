93a635766ca495404256355dc471a0117a6ce020
vc7 <vince78718@gmail.com>
2016-11-10 22:05:57 +0800

    新增 AccessTokenManager 實作

---
+++ protocolDemoSwift/Managers/AccessTokenManager.swift
@@ -0,0 +1,21 @@
+//
+//  AccessTokenManager.swift
+//  protocolDemoSwift
+//
+//  Created by vincent on 2016/11/10.
+//  Copyright © 2016年 vc7. All rights reserved.
+//
+
+import Foundation
+
+class AccessTokenManager: TokenManageable {
+    var token: String? {
+        return nil
+    }
+    
+    func update(token: String) {
+    }
+    
+    func reset() {
+    }
+}
