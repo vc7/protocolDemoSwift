576179baafaea5af05d250cb2f9cbec0d543688c
vc7 <vince78718@gmail.com>
2016-11-10 21:48:18 +0800

    Initial commit.

---
+++ protocolDemoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  protocolDemoSwift
+//
+//  Created by vincent on 2016/11/10.
+//  Copyright © 2016年 vc7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
